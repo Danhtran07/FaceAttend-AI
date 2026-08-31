4f4d51933c26f0f4d7de5b7d40e69e31e51c4c6a
Danhtran07 <tdanh589@gmail.com>
2026-08-31 21:56:30 +0700

    feat(backend): create employees table with migration script

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -13,6 +13,7 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 from app.core.config import settings
 from app.core.database import Base
 from app.models.user import User
+from app.models.employee import Employee
 
 
 # Alembic Config object
